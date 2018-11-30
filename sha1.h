7d51f376d28e1ab40eda572a131aca378c8354b7
Kwuaint <34888408+kwuaint@users.noreply.github.com>
2018-11-30 11:19:05 +0800

    update the descriptions of the function
    update the descriptions of the function

--- sha1.h
+++ sha1.h
@@ -8,9 +8,9 @@ extern "C" {
 #endif
 
 	/*
-	 * 功能：单向函数 SHA1
-	 * 输入：1. input ：输入消息
-	 *		 2. output：输出结果
+	 * name  ： sha1
+	 * params：1. input : input information
+	 *	   2. output: result
 	*/
 	void sha1(uint8_t *input, uint32_t inputLen, uint8_t *output);
 
